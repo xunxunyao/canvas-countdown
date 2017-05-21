de564d70368453656d2eb462994028bf13622ff3
yaoxunxun <xunxunyao@gmail.com>
2017-05-21 23:39:05 +0800

    计算倒计时长

--- countdown.js
+++ countdown.js
@@ -9,6 +9,10 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 //第一个数字左边距
 var MARGIN_LEFT = 30;
+//截止日期 2017/5/25 12:00:00
+const endTime = new Date(2017, 4, 25, 12, 0, 0);
+//距离截止日期多少秒
+var curShowTimeSeconds = 0;
 
 window.onload = function () {
 
@@ -18,15 +22,32 @@ window.onload = function () {
     canvas.width = WINDOW_WIDTH;
     canvas.height = WINDOW_HEIGHT;
 
+    curShowTimeSeconds = getCurrentShowTimeSeconds();
     render(context);
 
 };
 
+/**
+ * 当前距离截止日期所少秒
+ * @returns {number}
+ */
+function getCurrentShowTimeSeconds() {
+
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+
+    //毫秒转化成秒
+    ret = Math.round(ret / 1000);
+
+    return ret >= 0 ? ret : 0;
+
+}
+
 function render(cxt) {
 
-    var hours = 12;
-    var minutes = 34;
-    var second = 56;
+    var hours = parseInt(curShowTimeSeconds / 3600);
+    var minutes = parseInt((curShowTimeSeconds - hours * 3600) / 60);
+    var second = curShowTimeSeconds % 60;
 
     renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hours / 10), cxt);
     //第二个数字，数组是10*7的，相当于7个网格，每一个数字之间又有一点空隙，所以是7*2+1=15
@@ -43,6 +64,13 @@ function render(cxt) {
     renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(second % 10), cxt);
 }
 
+/**
+ * 绘制数字的每一个像素点
+ * @param x 每一个数字其实像素点的圆心x坐标
+ * @param y 每一个数字其实像素点的圆心y坐标
+ * @param num 要显示的数字
+ * @param cxt
+ */
 function renderDigit(x, y, num, cxt) {
 
     cxt.fillStyle = "rgb(0,102,153)";
@@ -52,7 +80,7 @@ function renderDigit(x, y, num, cxt) {
                 cxt.beginPath();
                 // 我们设定一个网格系统，每个像素点位于每个格子中心，格子的长宽为2*(RADIUS+1)
                 cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
-                    y + i * 2 * (RADIUS + 1)+(RADIUS + 1),//每个像素点圆心的位置
+                    y + i * 2 * (RADIUS + 1) + (RADIUS + 1),//每个像素点圆心的位置
                     RADIUS,//半径
                     0, 2 * Math.PI);//绘制一个完整的圆
                 cxt.closePath();
